683b40760ec845e9f9c5b910dc3addfe6d4ff803
Chloe VANACKER <chloe.vanacker@bbox.fr>
2016-11-13 19:10:24 +0100

    Ajout de la partie thread pour la conversion des videos

--- convertvideothread.cpp
+++ convertvideothread.cpp
@@ -1,7 +1,25 @@
 #include "convertvideothread.h"
+#include "header.h"
+#include "videosurface.h"
 
-ConvertVideoThread::ConvertVideoThread()
+//ConvertVideoThread::ConvertVideoThread(QObject *parent) : QThread(parent)
+
+ConvertVideoThread::ConvertVideoThread(QObject *parent, Controller* controller, QMediaPlayer* video_player, VideoSurface* video_surface) : QThread(parent)
 {
+    this->controller = controller;
+    this->video_player = video_player;
+    this->video_surface = video_surface;
+    this->video_surface->controller = this->controller;
+}
+
+
+void ConvertVideoThread::run()
+{
+    qDebug()<<QString("Thread debut");
+
+    this->video_surface->flag_convert = true;
+
+    qDebug()<<QString("Thread fin");
 
 }
 
--- convertvideothread.h
+++ convertvideothread.h
@@ -1,11 +1,28 @@
 #ifndef CONVERTVIDEOTHREAD_H
 #define CONVERTVIDEOTHREAD_H
 
+#include "header.h"
 
-class ConvertVideoThread
+class ConvertVideoThread : public QThread
 {
 public:
     ConvertVideoThread();
+
+public:
+    //explicit ConvertVideoThread(QObject *parent = 0);
+
+    explicit ConvertVideoThread(QObject *parent, Controller* controller, QMediaPlayer* video_player, VideoSurface* video_surface);
+
+    void run();
+
+public:
+
+    QMediaPlayer* video_player;
+    VideoSurface* video_surface;
+    Controller* controller;
+
+private:
+
 };
 
 #endif // CONVERTVIDEOTHREAD_H
